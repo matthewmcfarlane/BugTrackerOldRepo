37386fc926b532c0596151adf4c5ac4b2d658784
Adam Kidd <adamkidd3@gmail.com>
2022-02-23 12:46:41 +0000

    bug table skeleton and fetch to backend

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import JSONPretty from 'react-json-pretty';
+import BugTable from './BugTable';
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
 
   return (
-    isAuthenticated && ( 
+    isAuthenticated && (
+      <>
      <div>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
@@ -14,7 +16,9 @@ const Profile = () => {
         <JSONPretty data={user} />
         {/* {JSON.stringify(user, null, 2)} */}
       </div>
-      
+
+      <BugTable/>
+      </> 
     )
   )
 }
