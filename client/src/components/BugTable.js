d54c554a58c6aa88e8ebe1efc628d8f2928bc1bb
Adam Kidd <adamkidd3@gmail.com>
2022-02-23 16:16:02 +0000

    changes to bug table

--- client/src/components/BugTable.js
+++ client/src/components/BugTable.js
@@ -15,7 +15,7 @@ const BugTable = () => {
     })
 
     const getAllBugs = () => {
-        fetch('http://localhost:9090/bugs')
+        fetch('http://localhost:8080/bugs')
         .then(result => result.json())
         .then(data => setAllBugs(data));
     }
@@ -28,6 +28,13 @@ const BugTable = () => {
     }
 
     const bugRows = allBugs.map((bug, index) => {
+        let status = "Open";
+        if(bug.resolved){
+            status = "Closed";
+        }
+        else{
+            status = "Open";
+        }
         return(
             <tr>
                 <td className="px-6 py-4 whitespace-nowrap">
@@ -36,21 +43,22 @@ const BugTable = () => {
                             <img className="h-10 w-10 rounded-full" src={user.picture} alt=""/>
                         </div>
                         <div className="ml-4">
-                            <div className="text-sm font-medium text-gray-900">{user.nickname}</div>
-                            <div className="text-sm text-gray-500">{user.email}</div>
+                            <div className="text-sm font-medium text-gray-900">{bug.assignees[0].name}</div>
+                            <div className="text-sm text-gray-500">{bug.assignees[0].email}</div>
                         </div>
                     </div>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-900">Regional Paradigm Technician</div>
+                    <div className="text-sm text-gray-900">{bug.description}</div>
                     <div className="text-sm text-gray-500">Optimization</div>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
-                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"> Active </span>
+                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"> {bug.severity} </span>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">Admin</td>
                 <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                     <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
+                    {status}
                 </td>
             </tr>
         )
@@ -65,11 +73,11 @@ const BugTable = () => {
                         <table className="min-w-full divide-y divide-gray-200">
                             <thead className="bg-gray-50">
                                 <tr>
-                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Assignees</th>
                                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Description</th>
                                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Severity</th>
                                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
-                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date Posted</th>
+                                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
                                     <th scope="col" className="relative px-6 py-3">
                                         <span className="sr-only">Edit</span>
                                     </th>
