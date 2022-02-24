2f15ffeb26d0a89e7bf5a88a70f55f898d2d8011
Adam Kidd <adamkidd3@gmail.com>
2022-02-24 11:52:28 +0000

    add new bug

--- client/src/components/NewBugForm.js
+++ client/src/components/NewBugForm.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
+import postBug from "../services/BugsService";
 
 const NewBugForm = () => {
     const [allUsers, setAllUsers] = useState([]);
+    const [formData, setFormData] = useState({})
 
     useEffect(() => {
         getAllUsers();
@@ -13,9 +15,31 @@ const NewBugForm = () => {
         .then(data => setAllUsers(data))
     }
 
+    const onChange = (event) => {
+        formData[event.target.id] = event.target.value;
+        setFormData(formData);
+    }
+
+    // const onSubmit = (event) => {
+    //     event.preventDefault();
+    //     postBug(formData);
+    // }
+
     return(
-        <form>
-            <label></label>
+        <form onSubmit={onSubmit} method="post">
+            <label htmlFor="description">Bug Description:</label>
+            <input onChange={onChange} type="text" id="description" required/>
+
+            <label htmlFor="severity">Severity:</label>
+            <select onChange={onChange} id="severity" required>
+                <option value="high">High</option>
+                <option value="medium">Medium</option>
+                <option value="low">Low</option>
+            </select>
+
+            <input type="submit" value="Save" id="save"/>
         </form>
     )
 }
+
+export default NewBugForm;
