ae52997eb00e6faf931d204af325e89f6401e432
Adam Kidd <adamkidd3@gmail.com>
2022-02-28 12:57:40 +0000

    sort and filter functions

---
+++ client/src/services/sortAndFilter.js
@@ -0,0 +1,33 @@
+export const filterByActive = (bugList, Active = true) => {
+    return bugList.filter((bug) => bug.active === Active);
+}
+
+export const filterByPriority = (bugList, priority="high") => {
+    return bugList.filter((bug) => bug.priority === priority);
+}
+
+export const filterByUser = (bugList, userAuth0Sub) => {
+    const filteredBugList = [];
+    for(bug of bugList){
+        if(bug.reporter.auth0Sub === userAuth0Sub){
+            filteredBugList.push(bug);
+        }
+        else{
+            for(assignee of bug.assignees){
+                if(assignee.auth0Sub === userAuth0Sub){
+                    filteredBugList.push(bug);
+                }
+            }
+        };
+    }
+    return filteredBugList;
+}
+
+export const sortByDate = (bugList, newestFirst = true) => {
+    if(newestFirst){
+        return bugList.sort((a, b) => Date.parse(a.dateReported) - Date.parse(b.dateReported));
+    }
+    else{
+        return bugList.sort((a, b) => Date.parse(b.dateReported)  - Date.parse(a.dateReported));
+    }
+}
