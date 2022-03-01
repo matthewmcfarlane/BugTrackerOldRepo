aff49060599fc285e2a47ccbccd585ecd7afd732
Adam Kidd <adamkidd3@gmail.com>
2022-03-01 12:23:22 +0000

    moved assignee elements to new file

---
+++ client/src/components/AssigneeElements.js
@@ -0,0 +1,16 @@
+const AssigneeElements = ({ bug }) => {
+    return bug.assignees.map((assignee, index) => {
+      return(
+        <div key={index}>
+          <div className="text-sm font-medium text-gray-900">
+            {assignee.name}
+          </div>
+          <div className="text-sm text-gray-500">
+            {assignee.email}
+          </div>
+        </div>
+      )
+    });
+  }
+
+export default AssigneeElements;
